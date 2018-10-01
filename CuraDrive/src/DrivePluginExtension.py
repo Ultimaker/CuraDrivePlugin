aa9df4e3b3f143c35d03ffe46509c5fcb9ef7fcd
ChrisTerBeke <c.terbeke@ultimaker.com>
2018-10-01 17:18:27 +0200

    Some fixes for current Cura version

--- CuraDrive/src/DrivePluginExtension.py
+++ CuraDrive/src/DrivePluginExtension.py
@@ -1,12 +1,13 @@
 # Copyright (c) 2017 Ultimaker B.V.
 import os
 from datetime import datetime
-from typing import Optional, TYPE_CHECKING
+from typing import Optional
 
 from PyQt5.QtCore import QObject, pyqtSlot, pyqtProperty, pyqtSignal
 
 from UM.Extension import Extension
 from UM.Message import Message
+from cura.API import CuraAPI
 
 from .Settings import Settings
 from .DriveApiService import DriveApiService
@@ -46,7 +47,15 @@ class DrivePluginExtension(QObject, Extension):
 
         # Initialize services.
         self._preferences = self._application.getPreferences()
-        self._cura_api = self._application.getCuraAPI()
+
+        if hasattr(self._application, "getCuraAPI"):
+            # From Cura 3.6 onwards, the application has an instance of the API to use.
+            self._cura_api = self._application.getCuraAPI()
+        else:
+            # Prior to Cura 3.6, application did not hold an instance of the API ready to use.
+            # We must create our own instance here.
+            self._cura_api = CuraAPI()
+
         self._drive_api_service = DriveApiService(self._cura_api)
 
         # Attach signals.
